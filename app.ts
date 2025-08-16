e3078356ca954dbb47dfb786fe9056cfdfabf783
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-08-16 22:10:02 +0100

    feat: port added

--- app.ts
+++ app.ts
@@ -23,6 +23,8 @@ io.on("connection", (socket) => {
   });
 });
 
-httpServer.listen(4000, () => {
-  console.log("✅ Socket.IO server running at http://localhost:4000");
+const PORT = process.env.PORT || 4000; // ✅
+
+httpServer.listen(PORT, () => {
+  console.log(`✅ Socket.IO server running on port ${PORT}`);
 });
